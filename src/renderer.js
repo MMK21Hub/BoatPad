0f20a883038be978573a3fa273d3a236d6182ecf
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-02-21 11:30:08 +0000

    App: Add acorn

--- src/renderer.js
+++ src/renderer.js
@@ -7,6 +7,7 @@ const {
 const remote = require("electron").remote
 const fs = require("fs")
 const path = require("path")
+const acorn = require("acorn")
 
 bp.window.currentWindow = remote.getCurrentWindow()
 
