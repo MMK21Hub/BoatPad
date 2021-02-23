7b52123852d4c0dcff41d48c357d8d138860d4ac
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-02-23 13:08:38 +0000

    RPC: Add rpc.details.set and rpc.details.remove

--- scripts/discord-RP.js
+++ scripts/discord-RP.js
@@ -36,6 +36,31 @@ module.exports = {
         // Take a note of the current time:
         const startTimestamp = new Date()
 
+        // Let users enter a custom status:
+        let details = null
+        bp.commands.register(
+            "rpc.details.set",
+            "Rich presence: Set details",
+            "handled",
+            (args) => {
+                details = args[0]
+                // Try to update ASAP:
+                setActivity()
+            },
+            1,
+        )
+        bp.commands.register(
+            "rpc.details.remove",
+            "Rich presence: Reset details",
+            "handled",
+            (args) => {
+                details = null
+                // Try to update ASAP:
+                setActivity()
+            },
+            0,
+        )
+
         // Work out what status to show:
         if (bp.view.zen) {
             status = "In popout window"
@@ -44,20 +69,28 @@ module.exports = {
         }
 
         async function setActivity() {
-            rpc.setActivity({
-                state: status,
-                startTimestamp,
-                instance: false,
-            })
+            if (details) {
+                rpc.setActivity({
+                    details,
+                    state: status,
+                    startTimestamp,
+                })
+            } else {
+                rpc.setActivity({
+                    state: status,
+                    startTimestamp,
+                    instance: false,
+                })
+            }
         }
 
         rpc.on("ready", () => {
             // Start by sending the activity to Discord:
             setActivity()
-            // Update the activity every 15 secs:
+            // Update the activity every minute:
             setInterval(() => {
                 setActivity()
-            }, 15000)
+            }, 1 * 60 * 1000)
         })
 
         // Log in to Discord RPC:
